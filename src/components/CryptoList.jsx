6fa11938e476a2ffc56ae9dec75ad3d850034160
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-03-14 01:28:46 +0530

    Basic done

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,35 +1,38 @@
-import Input from "./Input.jsx"
+import { useState } from "react";
+import Input from "./Input.jsx";
+import CryptoItem from "./CryptoItem.jsx";
 
 export default function CryptoList({listData}) {
 
+    const [search, setSearch] = useState('');
+
     const fetched = listData ? true : false;
 
-    const redClass = "text-red-600";
-    const greenClass = "text-green-600";
+
+    const searchChangeHandler = (event) => {
+        setSearch(event.target.value);
+        console.log(search);
+    }
+
+    const filteredList = listData?.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
     const mainContent = (
         <>
-            <div className="my-3 flex justify-center items-center text-left flex-row gap-20 border-2 border-gray-400 rounded-full p-2">
+            <div className="my-3 flex justify-center items-center text-left flex-row gap-20 border-2 border-white rounded-full p-2">
                 <span className="flex-1/12"></span>
-                <span className="flex-2/12">Name</span>
+                <span className="flex-3/12">Name</span>
                 <span className="flex-3/12">Price</span>
-                <span className="flex-3/12">Market Cap</span>
+                <span className="flex-2/12">Market Cap</span>
                 <span className="flex-3/12">Change (24hr)</span>
             </div>
-            {listData?.map((item, index) => <div key={index} className="my-3 flex justify-center items-center flex-row gap-20 border-2 border-gray-400 rounded-full p-2">
-                <span className="flex-1/12"><img className="w-10 h-10" src={item.image} /></span>
-                <span className="flex-2/12">{item.name}</span>
-                <span className="flex-3/12">{item.current_price} USD</span>
-                <span className="flex-3/12">{(item.market_cap/1000000000).toFixed(2)} B</span>
-                <span className={`flex-3/12 ${item.price_change_percentage_24h > 0 ? greenClass : redClass}`}>{(item.price_change_percentage_24h.toFixed(2))} %</span>
-            </div>)} 
+            {filteredList?.map((item, index) => <CryptoItem itemData={item} key={index} />)} 
         </>
     )
 
     return (
-        <div className="bg-gray-950 h-auto w-full py-8 flex flex-col m-auto">
+        <div className="bg-gray-950 h-auto py-8 flex flex-col m-auto">
             <div className="w-7/10 mx-auto">
-                <Input type="text">
+                <Input onSearch={(event) => searchChangeHandler(event)} type="text">
                     Search
                 </Input>
             </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,9 @@
-export default function Input({type, children}) {
+
+
+export default function Input({type, children, onSearch}) {
+
+
     return (
-        <input className="text-white border-2 py-2 px-3 rounded-full w-2/6 hover:border-gray-400 duration-150" type={type} placeholder={children} />
+        <input onChange={(e) => onSearch(e)} className="text-white border-2 py-2 px-3 rounded-full w-2/6 hover:border-gray-400 duration-150" type={type} placeholder={children} />
     )
 }
