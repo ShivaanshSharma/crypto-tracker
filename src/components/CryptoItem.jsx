43f31a3a1c6f17eb7e858378b896489c1f4938f3
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-04-06 22:42:34 +0530

    font size

--- src/components/CryptoItem.jsx
+++ src/components/CryptoItem.jsx
@@ -5,7 +5,7 @@ export default function CryptoItem ({itemData}) {
     const greenClass = "text-green-600";
 
     return (
-        <div className="text-sm lg:text-base md:text-base sm:text-base my-3 flex items-center text-center flex-row border-2 border-white rounded-full p-2 hover:border-gray-400 gap-6">
+        <div className="text-xs lg:text-base md:text-base sm:text-base my-3 flex items-center text-center flex-row border-2 border-white rounded-full p-2 hover:border-gray-400 gap-6">
                 <span className=""><img className="min-w-10 min-h-10 max-w-10 max-h-10 rounded-full" src={itemData.image} /></span>
                 <span className="flex-2/6">{itemData.name}</span>
                 <span className="flex-1/6">{itemData.current_price.toFixed(2)} USD</span>
--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -41,7 +41,7 @@ export default function CryptoList({listData, onRefresh}) {
 
     const mainContent = (
         <>
-            <div className="text-sm lg:text-base md:text-base sm:text-base font-bold flex flex-row border-2 text-center border-white rounded-full p-2 gap-6 items-center">
+            <div className="text-xs lg:text-base md:text-base sm:text-base font-bold flex flex-row border-2 text-center border-white rounded-full p-2 gap-6 items-center">
                 <span className="opacity-0">shshs</span>
                 <span className="flex-2/6">Name</span>
                 <span className="flex-1/6">Price</span>
