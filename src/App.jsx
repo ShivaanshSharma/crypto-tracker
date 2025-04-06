05eb24b3e4a047ff39243eaa019a6fa176db126f
Shivaansh Sharma <shivaanshsharma2001@gmail.com>
2025-04-06 22:37:07 +0530

    a lot of small improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import CryptoList from "./components/CryptoList"
 import Header from "./components/Header"
 import { useEffect, useState } from "react"
-import axios from "axios"
 import Content from "./components/Content"
 
 function App() {
 
   const [list, setList] = useState(undefined);
+  const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-
-    const fetch = () => {
-      axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h").then((response) => setList(response.data))
+    async function fetchData() {
+      const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h');
+      const responseData = await response.json();
+      setList(responseData);
     }
 
-    if (list === undefined) {
-      fetch();
-    }
+    fetchData();
 
-    setInterval(() => {
-      fetch();
-    }, 60000); 
-  }, []);
+  }, [refresh]);
+
+  const refreshHandler = () => {
+    setRefresh(prev => !prev);
+  }
 
  return (
     <>
       <Header />
       <Content />
-      <CryptoList listData={list} />
+      <CryptoList listData={list} onRefresh={refreshHandler} />
     </>
   )
 }
--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -2,45 +2,42 @@ import { useEffect, useState } from "react";
 import Input from "./Input.jsx";
 import CryptoItem from "./CryptoItem.jsx";
 
-export default function CryptoList({listData}) {
+export default function CryptoList({listData, onRefresh}) {
 
     console.log(listData);
 
     const [search, setSearch] = useState('');
-    const [showList, setShowList] = useState();
+    const [showList, setShowList] = useState('allCoins');
+    const [disable, setDisable] = useState(false);
+    const [fetching, setFetching] = useState(true);
 
     useEffect(() => {
         setShowList(listData);
+        setFetching(false);
     }, [listData])
-
-    const fetched = listData ? true : false;
 
     const searchChangeHandler = (event) => {
         setSearch(event.target.value);
         console.log(search);
     }
 
-    const topHandler = () => {
-        let newList = listData.sort((a, b) => b.market_cap - a.market_cap).slice(0, 10);
-        setShowList(newList);
+    let newList = listData;
+
+    if (showList === 'allCoins') {
+        newList = listData;
+    } else if (showList === 'top10Coins') {
+        newList = listData.sort((a, b) => b.market_cap - a.market_cap).slice(0, 10);
+    } else if (showList === 'trending') {
+        newList = listData.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h).slice(0, 10);
+    } else if (showList === 'below1') {
+        newList = listData.filter((item) => item.current_price <= 1);
     }
 
-    const belowHandler = () => {
-        let newList = listData.filter((item) => item.current_price <= 1);
-        setShowList(newList);
+    const selectionChangeHandler = (event) => {
+        setShowList(event.target.value);
     }
 
-    const trendingHandler = () => {
-        let newList = listData.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h).slice(0, 10);
-        setShowList(newList);
-    }
-
-    const allCoinsHandler = () => {
-        let newList = listData;
-        setShowList(newList);
-    }
-
-    let filteredList = showList?.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
+    let filteredList = newList?.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
     const mainContent = (
         <>
@@ -55,21 +52,36 @@ export default function CryptoList({listData}) {
         </>
     )
 
+    const refreshHandler = async () => {
+        setFetching(true);
+        setDisable(true);
+        setTimeout(() => {
+            setDisable(false);
+        }, 60000);
+        onRefresh();
+    }
+
     return (
         <div className="bg-gray-950 h-auto flex flex-col mx-auto gap-6">
             <div className="w-9/10 lg:w-7/10 md:w-7/10 sm:w-7/10 mx-auto text-white flex flex-col lg:flex-row md:flex-row sm:flex-row gap-3 justify-between">
                 <Input onSearch={(event) => searchChangeHandler(event)} type="text">
                     Search
                 </Input>
-                <span className="flex flex-col lg:flex-row md:flex-row sm:flex-row gap-3">
-                    <button onClick={topHandler} className="cursor-pointer duration-150 border-2 rounded-xl p-3 hover:bg-blue-950 hover:text-white">Top 10 coins</button>
-                    <button onClick={belowHandler} className="cursor-pointer duration-150 border-2 rounded-xl p-3 hover:bg-blue-950 hover:text-white">Below 1$</button>
-                    <button onClick={trendingHandler} className="cursor-pointer duration-150 border-2 rounded-xl p-3 hover:bg-blue-950 hover:text-white">Trending</button>
-                    <button onClick={allCoinsHandler} className="cursor-pointer duration-150 border-2 rounded-xl p-3 hover:bg-blue-950 hover:text-white">All coins</button>
+                <span className="flex flex-row justify-between lg:flex-row md:flex-row sm:flex-row gap-3">
+
+                    <select className="cursor-pointer border-2 p-2 text-left rounded-full" name='selection' onChange={selectionChangeHandler}>
+                        <option className="bg-blue-950" value='allCoins'>All coins</option>
+                        <option className="bg-blue-950" value='trending'>Trending</option>
+                        <option className="bg-blue-950" value='top10Coins'>Top 10 coins</option>
+                        <option className="bg-blue-950"  value='below1'>Below 1$</option>
+                    </select>
+
+                    <button disabled={disable} onClick={refreshHandler}  className={`cursor-pointer duration-150 p-2 px-3 border-2 rounded-full hover:bg-blue-950 ${disable ? "opacity-25": null}`}>Refresh</button>
+                
                 </span>
             </div>
             <div className="w-10/10 lg:w-7/10 md:w-7/10 sm:w-9/10 mx-auto text-white my-5">
-                {fetched ? mainContent : <div className="border-2 rounded-full p-4 border-white text-center"><div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full mx-auto animate-spin"></div></div>}
+                {!fetching ? mainContent : <div className="border-2 rounded-full p-4 border-white text-center"><div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full mx-auto animate-spin"></div></div>}
             </div>
         </div>
     )
